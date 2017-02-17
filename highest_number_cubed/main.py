b88d11ae3258fbc7552a4b9508c25feed19d5571
Brian Park <jwpark05@gmail.com>
2017-02-17 02:19:15 +0000

    Initial commit

--- highest_number_cubed/main.py
+++ highest_number_cubed/main.py
@@ -1,5 +1,7 @@
 """This is the entry point of the program."""
 
-
 def highest_number_cubed(limit):
-    pass
+    count = 0 
+    while (count ** 3) < limit:
+        count += 1
+    return count -1 
